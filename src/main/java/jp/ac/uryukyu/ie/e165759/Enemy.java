81a2a88bba840e2890b3e059fee54590c32328e7
Hiroyuki Tamae <e165759@ie.u-ryukyu.ac.jp>
2017-01-15 15:54:26 +0900

    Step1. Add files.

---
+++ src/main/java/jp/ac/uryukyu/ie/e165759/Enemy.java
@@ -0,0 +1,25 @@
+package jp.ac.uryukyu.ie.e165759;
+
+/**
+ * Created by e165759 on 2016/11/23.
+ */
+
+/**
+ * 敵クラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Enemy extends LivingThing {
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name,maximumHP,attack);
+    }
+}
